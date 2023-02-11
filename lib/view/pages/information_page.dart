b0bed9c3a7a2b59f37500343cbf8977357fdd422
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-11 18:53:27 -0300

    refactor: finished information page

--- lib/view/pages/information_page.dart
+++ lib/view/pages/information_page.dart
@@ -9,8 +9,42 @@ class InformationPage extends StatefulWidget {
 }
 
 class _InformationPageState extends State<InformationPage> {
+  final currentYear = DateTime.now().year;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(widget.title)));
+    return SafeArea(
+      child: Scaffold(
+          resizeToAvoidBottomInset: true,
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'FindYourPet',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Versão 1.0.0',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(height: 20),
+                    Image.asset('assets/images/pet_icon_green.png'),
+                    const SizedBox(height: 20),
+                    Text(
+                      '2022 - $currentYear Kalify Inc.',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
   }
 }
