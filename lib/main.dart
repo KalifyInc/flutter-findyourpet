3f956044c96249837dc1640d6b1a113c5fc69254
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-11 18:51:59 -0300

    refactor: update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import '../assets/firebase_options.dart';
+import 'package:mobile_find_your_pet/themes/app_theme.dart';
+import '../config/firebase_options.dart';
 import 'view/pages/start_page.dart';
 
 void main() async {
@@ -8,19 +9,25 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Find your pet',
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-        ),
-        home: StartPage());
+        debugShowCheckedModeBanner: true,
+        theme: ThemeData(primarySwatch: Colors.teal, fontFamily: 'Poppins'),
+        darkTheme: AppTheme().darkTheme, // standard dark theme
+        themeMode: ThemeMode.system, // device controls theme),
+        home: const StartPage());
   }
 }
