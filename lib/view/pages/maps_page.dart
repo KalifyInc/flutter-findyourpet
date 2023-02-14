b420e12cdcff50185169e91cb3d171e9ad225ec3
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-13 22:27:06 -0300

    refactor: add mapcomponent

--- lib/view/pages/maps_page.dart
+++ lib/view/pages/maps_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../components/map.dart';
+
 class MapsPage extends StatefulWidget {
-  const MapsPage({super.key, required this.title});
-  final String title;
+  const MapsPage({super.key});
 
   @override
   State<MapsPage> createState() => _MapsPageState();
@@ -12,13 +13,18 @@ class _MapsPageState extends State<MapsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'FindYourPet',
-            style: TextStyle(fontSize: 25),
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          'FindYourPet',
+          style: TextStyle(fontSize: 25),
         ),
-        body: Center(child: Text(widget.title)));
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+          child: Container(
+        height: 1000,
+        child: MapComponent(),
+      )),
+    );
   }
 }
