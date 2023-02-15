7d1995714402bf0fa3a66f5ade25753bd564610a
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-15 09:48:56 -0300

    refactor: refactor code, delete screen navigation

---
+++ lib/app/view/pages/start_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_find_your_pet/app/view/pages/information_page.dart';
+import 'package:mobile_find_your_pet/app/view/pages/maps_page.dart';
+import 'package:mobile_find_your_pet/app/view/pages/register_page.dart';
+
+import '../components/bottom_navigation_bar.dart';
+import 'home_page.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  int currentIndex = 0;
+
+  List<Widget> bodyWidget = const [
+    HomePage(title: 'Home Page'),
+    RegisterPage(),
+    MapsPage(),
+    InformationPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: bodyWidget[currentIndex],
+      bottomNavigationBar: BottomNavigation(
+          currentIndex: currentIndex,
+          onTapController: (newIndex) {
+            setState(() {
+              currentIndex = newIndex;
+            });
+          }),
+    );
+  }
+}
