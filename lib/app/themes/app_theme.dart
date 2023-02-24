b36b53d8e8e9ef715fea3f44bc5f5a1319881f39
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-24 17:56:50 -0300

    refactor: refactor code

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -7,11 +7,6 @@ class AppTheme {
       appBarTheme: const AppBarTheme(
           color: Colors.black12,
           systemOverlayStyle: SystemUiOverlayStyle.light),
-
-      // inputDecorationTheme: InputDecorationTheme(
-      //   hintStyle: TextStyle(color: Colors.white12),
-      //   labelStyle: TextStyle(color: Colors.white10),
-      // ),
       brightness: Brightness.dark,
       canvasColor: const Color.fromRGBO(40, 42, 54, 1),
       bottomNavigationBarTheme: const BottomNavigationBarThemeData(
