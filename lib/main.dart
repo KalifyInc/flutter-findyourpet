7a2ddf2955f597a4bcd7b76a4f78c955f3eccd4d
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-26 12:55:20 -0300

    refactor: update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:FindYourPet/app/themes/app_theme.dart';
 import 'app/config/firebase_options.dart';
+import 'app/view/pages/error_page.dart';
+import 'app/view/pages/loading_page.dart';
 import 'app/view/pages/start_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+
   runApp(const App());
 }
 
@@ -20,14 +20,29 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> with WidgetsBindingObserver {
+  final Future<FirebaseApp> _inicializacao = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Find your pet',
-        debugShowCheckedModeBanner: true,
-        theme: ThemeData(primarySwatch: Colors.teal, fontFamily: 'Poppins'),
-        darkTheme: AppTheme().darkTheme, // standard dark theme
-        themeMode: ThemeMode.system, // device controls theme),
-        home: const StartPage());
+      title: 'Find your pet',
+      debugShowCheckedModeBanner: true,
+      theme: ThemeData(primarySwatch: Colors.teal, fontFamily: 'Poppins'),
+      darkTheme: AppTheme().darkTheme, // standard dark theme
+      themeMode: ThemeMode.system, // device controls theme),
+      home: FutureBuilder(
+        future: _inicializacao,
+        builder: (context, app) {
+          if (app.connectionState == ConnectionState.done) {
+            return const StartPage();
+          }
+
+          if (app.hasError) return const ErrorPage();
+
+          return const LoadingPage();
+        },
+      ),
+    );
   }
 }
