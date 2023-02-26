fa8954d09e47e5620caa440c5d75844c283612ed
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-25 22:41:33 -0300

    refactor: update navigator routes

---
+++ lib/app/controller/route_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../view/components/tab_navigator.dart';
+
+class RouteController {
+  String currentPage = 'HomePage';
+  int currentIndex = 0;
+
+  List<String> pageKeys = const [
+    'HomePage',
+    'RegisterPage',
+    'MapsPage',
+    'InformationPage',
+  ];
+
+  Map<String, GlobalKey<NavigatorState>> navigatorKeys = {
+    'HomePage': GlobalKey<NavigatorState>(),
+    'RegisterPage': GlobalKey<NavigatorState>(),
+    'MapsPage': GlobalKey<NavigatorState>(),
+    'InformationPage': GlobalKey<NavigatorState>(),
+    'ErrorPage': GlobalKey<NavigatorState>(),
+  };
+
+  Widget buildOffstageNavigator(String tabItem) {
+    return Offstage(
+      offstage: currentPage != tabItem,
+      child: TabNavigator(
+        navigatorKey: navigatorKeys[tabItem],
+        tabItem: tabItem,
+      ),
+    );
+  }
+}
---
+++ lib/app/view/components/tab_navigator.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../controller/route_controller.dart';
+import '../pages/home_page.dart';
+import '../pages/register_page.dart';
+import '../pages/maps_page.dart';
+
+import '../pages/information_page.dart';
+
+class TabNavigatorRoutes {
+  static const String home = '/';
+  static const String register = '/register';
+  static const String maps = '/maps';
+  static const String information = '/information';
+  static const String error = '/error';
+}
+
+class TabNavigator extends StatelessWidget {
+  const TabNavigator({required this.navigatorKey, required this.tabItem});
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final String tabItem;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child;
+
+    switch (tabItem) {
+      case 'HomePage':
+        child = const HomePage();
+        break;
+
+      case 'RegisterPage':
+        child = const RegisterPage();
+        break;
+
+      case 'MapsPage':
+        child = const MapsPage();
+        break;
+
+      case 'InformationPage':
+        child = const InformationPage();
+        break;
+
+      default:
+        child = const HomePage();
+    }
+
+    return Navigator(
+      key: navigatorKey,
+      onGenerateRoute: (routeSettings) {
+        return MaterialPageRoute(builder: (context) => child);
+      },
+    );
+  }
+}
