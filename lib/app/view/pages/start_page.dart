973af10a1d34ef5c1b8262c228ca1aef399f2d5b
Helen Cris <helencrisfernandes@hotmail.com>
2023-02-25 22:43:03 -0300

    refactor: update navigator form

--- lib/app/view/pages/start_page.dart
+++ lib/app/view/pages/start_page.dart
@@ -1,10 +1,5 @@
+import 'package:FindYourPet/app/controller/route_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:FindYourPet/app/view/pages/information_page.dart';
-import 'package:FindYourPet/app/view/pages/maps_page.dart';
-import 'package:FindYourPet/app/view/pages/register_page.dart';
-
-import '../components/bottom_navigation_bar.dart';
-import 'home_page.dart';
 
 class StartPage extends StatefulWidget {
   const StartPage({super.key});
@@ -14,26 +9,68 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
-  int currentIndex = 0;
+  RouteController routeController = RouteController();
 
-  List<Widget> bodyWidget = const [
-    HomePage(title: 'Home Page'),
-    RegisterPage(),
-    MapsPage(),
-    InformationPage(),
-  ];
+  void _selectTab(String tabItem, int index) {
+    if (tabItem == routeController.currentPage) {
+      routeController.navigatorKeys[tabItem]!.currentState!
+          .popUntil((route) => route.isFirst);
+    } else {
+      setState(() {
+        routeController.currentPage = routeController.pageKeys[index];
+        routeController.currentIndex = index;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: bodyWidget[currentIndex],
-      bottomNavigationBar: BottomNavigation(
-          currentIndex: currentIndex,
-          onTapController: (newIndex) {
-            setState(() {
-              currentIndex = newIndex;
-            });
-          }),
+    return WillPopScope(
+      onWillPop: () async {
+        final isFirstRouteInCurrentTab = !await routeController
+            .navigatorKeys[routeController.currentPage]!.currentState!
+            .maybePop();
+        if (isFirstRouteInCurrentTab) {
+          if (routeController.currentPage != 'HomePage') {
+            _selectTab('HomePage', 1);
+
+            return false;
+          }
+        }
+        // let system handle back button if we're on the first route
+        return isFirstRouteInCurrentTab;
+      },
+      child: Scaffold(
+        body: Stack(children: <Widget>[
+          routeController.buildOffstageNavigator('HomePage'),
+          routeController.buildOffstageNavigator('RegisterPage'),
+          routeController.buildOffstageNavigator('MapsPage'),
+          routeController.buildOffstageNavigator('InformationPage'),
+          routeController.buildOffstageNavigator('ErrorPage'),
+        ]),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: routeController.currentIndex,
+          onTap: (int index) {
+            // setState(() {
+            //   currentIndex = newIndex;
+            // });
+            _selectTab(routeController.pageKeys[index], index);
+          },
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.teal,
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          iconSize: 30,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.pets), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.app_registration), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.location_pin), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.info_outline), label: ''),
+          ],
+        ),
+      ),
     );
   }
 }
