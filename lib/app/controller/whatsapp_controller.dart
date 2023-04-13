b4305d65d84c458393a7c93ad9c106bec0f8bd52
Helen Cris <helencrisfernandes@hotmail.com>
2023-04-13 18:17:12 -0300

    refactor: add pre-message in the whats link

--- lib/app/controller/whatsapp_controller.dart
+++ lib/app/controller/whatsapp_controller.dart
@@ -4,9 +4,8 @@ import 'package:url_launcher/url_launcher.dart';
 class WhatsAppController {
   openWhatsapp(
       {required BuildContext context, required String telephoneNumber}) async {
-    final Uri whatsappURL =
-        Uri.parse("https://api.whatsapp.com/send?phone=$telephoneNumber");
-
+    final Uri whatsappURL = Uri.parse(
+        "https://api.whatsapp.com/send?phone=$telephoneNumber&text=Olá%2C+tudo+bom%3F+Vim+do+FindYourPet+e+estou+interessada+em+saber+mais+a+respeito+do+pet+que+está+no+anúncio+..");
     if (await canLaunchUrl(whatsappURL)) {
       await launchUrl(whatsappURL, mode: LaunchMode.externalApplication);
     } else {
