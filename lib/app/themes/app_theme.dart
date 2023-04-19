236f7dcb70145079a5b563417b5dcf566883aff7
Helen Cris <helencrisfernandes@hotmail.com>
2023-04-18 22:54:18 -0300

    refactor: add light theme, and value notifier

--- lib/app/themes/app_theme.dart
+++ lib/app/themes/app_theme.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AppTheme {
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.light);
+
+  get lightTheme =>
+      ThemeData(primarySwatch: Colors.teal, fontFamily: 'Poppins');
+
   get darkTheme => ThemeData(
       primarySwatch: Colors.teal,
+      fontFamily: 'Poppins',
       appBarTheme: const AppBarTheme(
           color: Colors.black12,
           systemOverlayStyle: SystemUiOverlayStyle.light),
