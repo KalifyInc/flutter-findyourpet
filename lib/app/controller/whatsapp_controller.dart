8d5fce52a56376dc30748ae5faa0ecfd19d5bab5
Helen Cris <helencrisfernandes@hotmail.com>
2023-06-30 22:55:26 -0300

    fix bug error url

--- lib/app/controller/whatsapp_controller.dart
+++ lib/app/controller/whatsapp_controller.dart
@@ -6,10 +6,10 @@ class WhatsAppController {
       {required BuildContext context, required String telephoneNumber}) async {
     final Uri whatsappURL = Uri.parse(
         "https://api.whatsapp.com/send?phone=$telephoneNumber&text=Olá%2C+tudo+bom%3F+Vim+do+FindYourPet+e+estou+interessada+em+saber+mais+a+respeito+do+pet+que+está+no+anúncio+..");
-    if (await canLaunchUrl(whatsappURL)) {
+    try {
       await launchUrl(whatsappURL, mode: LaunchMode.externalApplication);
-    } else {
-      throw 'Error';
+    } catch (e) {
+      throw 'Error $e';
     }
   }
 }
