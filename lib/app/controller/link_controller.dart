091f5ee6b8adf89359f263d5c2c069a6fbb997f5
Helen Cris <helencrisfernandes@hotmail.com>
2023-07-03 11:35:38 -0300

    created link controller

---
+++ lib/app/controller/link_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LinkController {
+  openLink({required BuildContext context, required String urlParam}) async {
+    Uri url = Uri.parse(urlParam);
+    try {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            'Erro: ${e.toString()}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.redAccent,
+        ),
+      );
+    }
+  }
+}
